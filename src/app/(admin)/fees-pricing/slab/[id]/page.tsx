60702effff8a2535395d6a427a9e680777bbaa6f
0xkid-root <prohackers698@gmail.com>
2026-09-22 12:37:32 +0530

    style: apply dark theme color fixes to page.tsx

--- src/app/(admin)/fees-pricing/slab/[id]/page.tsx
+++ src/app/(admin)/fees-pricing/slab/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { SlabFeeDetail } from "@/features/fees-pricing/components/slab/slab-fee-detail";
 
 export default function Page() {
-  return <SlabFeeDetail />;
+ return <SlabFeeDetail />;
 }
