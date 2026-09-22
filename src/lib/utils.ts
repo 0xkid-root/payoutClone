716e64657b6a42ba027c6f189d02e4a3bb4807c7
0xkid-root <prohackers698@gmail.com>
2026-09-22 12:47:43 +0530

    style: apply dark theme color fixes to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,13 +2,13 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+ return twMerge(clsx(inputs))
 }
 
 export function formatINR(amount: number) {
-  return new Intl.NumberFormat("en-IN", {
-    style: "currency",
-    currency: "INR",
-    maximumFractionDigits: 2,
-  }).format(amount);
+ return new Intl.NumberFormat("en-IN", {
+ style: "currency",
+ currency: "INR",
+ maximumFractionDigits: 2,
+ }).format(amount);
 }
