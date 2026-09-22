39c1315bdf7dc340d7e04b51c94ebf6342f94bce
0xkid-root <prohackers698@gmail.com>
2026-09-22 12:37:55 +0530

    style: apply dark theme color fixes to page.tsx

--- src/app/(admin)/payout-management/[id]/page.tsx
+++ src/app/(admin)/payout-management/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { PayoutDetailsPage } from "@/features/payout/components/payout-details-page";
 
 export default function PayoutDetailsRoute({ params }: { params: { id: string } }) {
-  return <PayoutDetailsPage id={params.id} />;
+ return <PayoutDetailsPage id={params.id} />;
 }
