fe1a7029bbec18afb64304064b4d180cb882cd60
0xkid-root <prohackers698@gmail.com>
2026-09-22 12:42:09 +0530

    style: apply dark theme color fixes to dashboard-page.tsx

--- src/features/dashboard/components/dashboard-page.tsx
+++ src/features/dashboard/components/dashboard-page.tsx
@@ -8,39 +8,39 @@ import { PayoutStatusDistribution } from "./payout-status-distribution";
 import { MerchantOverviewTable } from "./merchant-overview-table";
 
 export default function DashboardPage() {
-  return (
-    <div className="flex flex-col gap-8 pb-8">
-      {/* 1. Header Section */}
-      <DashboardHeader />
+ return (
+ <div className="flex flex-col gap-8 pb-8">
+ {/* 1. Header Section */}
+ <DashboardHeader />
 
-      {/* 2. KPI Cards */}
-      <DashboardKpiGrid />
+ {/* 2. KPI Cards */}
+ <DashboardKpiGrid />
 
-      {/* 3. Middle Section: Charts & Top Merchants */}
-      <div className="grid gap-5 lg:grid-cols-3">
-        {/* Left Chart */}
-        <div className="lg:col-span-2">
-          <PayoutVolumeChart />
-        </div>
-        
-        {/* Center List */}
-        <div className="lg:col-span-1">
-          <TopMerchants />
-        </div>
-      </div>
+ {/* 3. Middle Section: Charts & Top Merchants */}
+ <div className="grid gap-5 lg:grid-cols-3">
+ {/* Left Chart */}
+ <div className="lg:col-span-2">
+ <PayoutVolumeChart />
+ </div>
+ 
+ {/* Center List */}
+ <div className="lg:col-span-1">
+ <TopMerchants />
+ </div>
+ </div>
 
-      {/* 4. Bottom Section: Data Table & Donut */}
-      <div className="grid gap-5 lg:grid-cols-3">
-        {/* Table (2/3 width) */}
-        <div className="lg:col-span-2">
-          <MerchantOverviewTable />
-        </div>
-        
-        {/* Donut Chart (1/3 width) */}
-        <div className="lg:col-span-1">
-          <PayoutStatusDistribution />
-        </div>
-      </div>
-    </div>
-  );
+ {/* 4. Bottom Section: Data Table & Donut */}
+ <div className="grid gap-5 lg:grid-cols-3">
+ {/* Table (2/3 width) */}
+ <div className="lg:col-span-2">
+ <MerchantOverviewTable />
+ </div>
+ 
+ {/* Donut Chart (1/3 width) */}
+ <div className="lg:col-span-1">
+ <PayoutStatusDistribution />
+ </div>
+ </div>
+ </div>
+ );
 }
